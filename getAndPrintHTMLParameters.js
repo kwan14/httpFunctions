36381b98d7e3ab99f560f45203110bfe696b432b
kwan14 <mendel.kwan@telus.net>
2017-09-11 19:28:49 +0000

    Added URL parameter to getAndPrintHTML

---
+++ getAndPrintHTMLParameters.js
@@ -0,0 +1,24 @@
+
+
+const https = require('https');
+
+function getAndPrintHTML (options) {
+
+  var httpResponseData = "";
+  https.get(options, (response) => {
+    response.setEncoding("utf8");
+    response.on('data', (chunk) => {
+      httpResponseData += chunk;
+    });
+    response.on('end', () => {
+      console.log(httpResponseData);
+    })
+  })
+}
+
+ const requestOptions = {
+    host : "sytantris.github.io",
+    path : "/http-examples/step2.html"
+  };
+
+getAndPrintHTML(requestOptions);
