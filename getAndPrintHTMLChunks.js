c070723f5d404abcc41e2762e65bb91e85cb9b0a
kwan14 <mendel.kwan@telus.net>
2017-09-11 18:06:59 +0000

    Initial commit

---
+++ getAndPrintHTMLChunks.js
@@ -0,0 +1,17 @@
+
+const https = require("https");
+
+function getAndPrintHTMLChunks () {
+
+  const requestOptions = {
+    host: "sytantris.github.io",
+    path: "/http-examples/step1.html"
+  };
+
+  https.get(requestOptions, (response) => {
+    response.setEncoding('utf8');
+    response.on('data', (chunk) => {
+      console.log(chunk);
+    })
+  })
+}
